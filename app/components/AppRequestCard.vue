df7b66fd6307f692d7a96d2e3f7283b1a39ac11a
dongaber <boozy044@gmail.com>
2026-06-02 13:23:12 +0300

    feat(card): show from/to addresses for TRANSPORTATION type

--- app/components/AppRequestCard.vue
+++ app/components/AppRequestCard.vue
@@ -8,6 +8,13 @@
     </div>
 
     <div class="card__body">
+      <div v-if="fromAddress" class="card__row">
+        <svg class="card__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <circle cx="12" cy="12" r="4" fill="currentColor"/>
+          <circle cx="12" cy="12" r="8" fill="none" stroke="currentColor" stroke-width="2"/>
+        </svg>
+        <span class="card__address">{{ fromAddress }}</span>
+      </div>
       <div class="card__row">
         <svg class="card__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <path fill="currentColor" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7m0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5"/>
@@ -173,6 +180,10 @@ const paymentLabels: Record<string, string> = {
 const typeLabel = computed(() => typeLabels[props.request.type] ?? props.request.type)
 const statusLabel = computed(() => statusLabels[props.request.status] ?? props.request.status)
 const paymentLabel = computed(() => paymentLabels[props.request.paymentType] ?? props.request.paymentType)
+
+const fromAddress = computed(() =>
+  props.request.transportation?.fromAddress ?? null
+)
 
 const mainAddress = computed(() => {
   const r = props.request
